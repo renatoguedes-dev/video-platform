ccebca404194aed4b5c1f8c0bd6c653d6ada7082
Renato Guedes <105191060+renatoguedes-dev@users.noreply.github.com>
2025-04-29 21:32:24 -0300

    first commit

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div>logo</div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Pagina Inicial</h1>
+    </div>
+  );
+}
